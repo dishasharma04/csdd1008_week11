5af771ea84ad5d2019f1874ba38b6bd4a96edd24
sachindhamija <sdhamija728@gmail.com>
2024-07-11 18:50:54 -0400

    API server and http handling

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// go mod init github.com/sojoudian/SimpleAPIServer
 func main() {
-	fmt.Println("Hello Class CSDD 1008!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	log.Println("Starting server on :3001")
+	err := http.ListenAndServe(":3001", mux)
+	if err != nil {
+		log.Fatalf("Error starting server: %s\n", err)
+	}
 }
